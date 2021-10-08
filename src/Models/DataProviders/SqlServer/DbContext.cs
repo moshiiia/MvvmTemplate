ed7f32e6384d2d52dbe32c88d4771f28dc50ad43
ivkinama <ivkinama@mer.ci.nsu.ru>
2021-10-08 14:11:38 +0700

    добавили первого поставщика данных SqlServer

---
+++ src/Models/DataProviders/SqlServer/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+
+namespace Models.DataProviders.SqlServer
+{
+    class DbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) 
+        {
+            dbContextOptionsBuilder.UseSqlServer(@"Data Source=DBSRV\MSSQL2021;Initial Catalog=MvvmTemplateDBSVI;Integrated Security=True");
+
+        }
+    }
+}
