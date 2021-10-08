0eb24266d9a6fda6823fcb7fad661d0573b56601
ivkinama <ivkinama@mer.ci.nsu.ru>
2021-10-08 13:32:13 +0700

    добавили сущности

---
+++ src/Models/Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    class Course
+    {
+        public Guid Id { get; init; }
+        public string Name { get; set; }
+        public Guid StudentId { get; set; }
+        public virtual IList<Student> Students { get; set; } = new List<Student>();
+    }
+}
---
+++ src/Models/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    class Student
+    {
+        public Guid Id { get; init; }
+        public string Name { get; set; }
+        public Guid CourseId { get; set; }
+        public virtual IList<Course> Courses { get; set; } = new List<Course>();
+    }
+}
